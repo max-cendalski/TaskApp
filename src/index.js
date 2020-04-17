7ef99c43b0c52ec0c8a215aac584267ad82e588e
Korneliu <olbiton@gmail.com>
2020-04-16 21:40:16 -0700

    added router folder for endpoints

--- src/index.js
+++ src/index.js
@@ -1,91 +1,20 @@
 const express = require('express')
-const app = express()
 require('./db/mongoose')
 const User = require('./db/models/user')
 const port = process.env.PORT || 3000
+const userRouter = require('./db/routers/user')
 
+const app = express()
 app.use(express.json())
+app.use(userRouter)
 
-app.post('/users', async(req, res) => {
-    const user = new User(req.body)
 
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
 
-app.get('/users/:id', async(req, res) => {
-    const _id = req.params.id
 
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
 
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
-app.get('/users', async(req, res) => {
-
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.patch('/users/:id', async(req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, useFindAndModify: false })
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.delete('/users/', async(req, res) => {
-    try {
-        const users = await User.deleteMany({})
-        res.send(users)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.delete('/users/:id', async(req, res) => {
-
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
 
 
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+    console.log('Server is up on port ' + port + ' and running')
 })
